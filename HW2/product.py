86664ec6d1b1a78e16c55fabb60d823211961f63
Jaeik Oh <Daheun@Jaeiks-Air.home>
2017-06-03 14:14:00 -0400

    Finished Hw 2

---
+++ HW2/product.py
@@ -0,0 +1,8 @@
+total = 1
+i = 1
+while i <= 20:
+    total *= i
+    i += 1
+print(total)
+for i in range(20):
+    print(i)
---
+++ HW2/sum.py
@@ -0,0 +1,8 @@
+total = 0
+i = 1
+while i <= 1000:
+    total += i
+    i += 1
+print(total)
+for i in range(1000):
+    print(i)
